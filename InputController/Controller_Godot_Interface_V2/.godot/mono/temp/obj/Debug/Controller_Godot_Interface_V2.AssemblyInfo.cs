390d17fdf519081107c966590f92d75d0dbf6bd9
SchaarNikita <211wita20@o365.htl-leoben.at>
2025-08-25 16:10:02 +0200

    Bell Bearing Hunter | Compose Main OST Theme in FLStudion and Export as .wav

--- InputController/Controller_Godot_Interface_V2/.godot/mono/temp/obj/Debug/Controller_Godot_Interface_V2.AssemblyInfo.cs
+++ InputController/Controller_Godot_Interface_V2/.godot/mono/temp/obj/Debug/Controller_Godot_Interface_V2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Controller_Godot_Interface_V2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ea1c3a60c44500122a9111131c3ece45213b11d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fb5694af7137d00ea174e20a45084a1dc02da03")]
 [assembly: System.Reflection.AssemblyProductAttribute("Controller_Godot_Interface_V2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Controller_Godot_Interface_V2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
