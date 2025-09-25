97c5f9c3d94c326ae75e678666b1680b58d449c9
kiesewitz <kiesewitz@gmail.com>
2025-09-25 12:10:04 +0200

    Yhorm the Giant | Add documentation docs in german and english

--- InputController/Controller_Godot_Interface_V2/CommunicationInterface.cs
+++ InputController/Controller_Godot_Interface_V2/CommunicationInterface.cs
@@ -20,7 +20,7 @@ public partial class CommunicationInterface : Node3D
 		text = GetNode<RichTextLabel>("RichTextLabel");
 		turnStick = GetNode<MeshInstance3D>("TurnStick");
 		text.Text = "Hallo";
-		serialPort = new SerialPort("COM4", 9600, Parity.None, 8, StopBits.One);
+		serialPort = new SerialPort("COM6", 9600, Parity.None, 8, StopBits.One);
 		serialPort.DtrEnable = true;
 		serialPort.RtsEnable = true;
 		serialPort.Open();
